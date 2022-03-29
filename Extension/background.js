48d2c96fe8e58cd6db43bc3b9f8ebe7d57d23205
Max <max@xwega.com>
2022-03-29 23:31:48 +0100

    v1.0.2

--- Extension/background.js
+++ Extension/background.js
@@ -43,7 +43,10 @@ function SetHeaderRules()
             removeRuleIds: [ 
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
                 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-                41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60 ]
+                41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+                101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120,
+                121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140,
+                141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160 ]
         }, () => 
         {
             chrome.action.setIcon(
@@ -93,9 +96,21 @@ function SetHeaderRules()
                                         },
                                         "condition": { "urlFilter": header.url }
                                     },
+                                    {
+                                        "id": (header.index + 100),
+                                        "priority": 2,
+                                        "action": 
+                                        {
+                                            "type": "modifyHeaders",
+                                            "requestHeaders": 
+                                            [
+                                                { "header": header.name, "operation": header.action, "value": header.value }
+                                            ]
+                                        },
+                                        "condition": { "urlFilter": header.url, resourceTypes: ["main_frame"] }
+                                    },
                                 ]
                             });
-                    
                     }
         
                     if (header.action == "remove")
@@ -116,6 +131,19 @@ function SetHeaderRules()
                                             ]
                                         },
                                         "condition": { "urlFilter": header.url }
+                                    },
+                                    {
+                                        "id": (header.index + 2),
+                                        "priority": 2,
+                                        "action": 
+                                        {
+                                            "type": "modifyHeaders",
+                                            "requestHeaders": 
+                                            [
+                                                { "header": header.name, "operation": header.action, }
+                                            ]
+                                        },
+                                        "condition": { "urlFilter": header.url, resourceTypes: ["main_frame"] }
                                     },
                                 ]
                             });
